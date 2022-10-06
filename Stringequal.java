bef4c6afbbbad2be28248ebe8f9a2e0d26d80b1a
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-06 13:37:02 +0530

    Added:Two String Equal or Not

---
+++ Stringequal.java
@@ -0,0 +1,14 @@
+package com.BridgeLabz.Day1PracticeProblem;
+
+public class Stringequal {
+
+	public static void main(String[] args) {
+		String A="John";  
+		String B="John";
+		String C="Smith";
+		
+		System.out.println(A.equals(B));  //true
+		System.out.println(A.equals(C));  //false
+	}
+
+}
